d0090c904dcd8ac9655a65eb34641ef966f82e11
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-06-03 12:35:47 -0500

    fixed index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css';
+import './reset.css';
+
+import Game from './components/game';
+
+ReactDOM.render(<Game />, document.getElementById('root'));

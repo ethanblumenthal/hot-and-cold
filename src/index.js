0e170deb0f50fa282ecc3934a8823485cdf03f99
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-06-03 16:30:35 -0500

    feedback component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './index.css';
 import './reset.css';
-
 import Game from './components/game';
 
 ReactDOM.render(<Game />, document.getElementById('root'));

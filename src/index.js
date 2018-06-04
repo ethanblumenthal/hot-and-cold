5ae8660923838222efde0e274d50a62dbc2c66ae
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-06-03 22:48:27 -0500

    added provider to index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import './index.css';
 import './reset.css';
+import store from './store';
 import Game from './components/game';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Game />
+  </Provider>,
+   document.getElementById('root')
+);

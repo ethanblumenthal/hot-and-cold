8afbf6a68016e65115ada77c0989977dd90c65a8
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-06-04 00:15:38 -0500

    fixed module import error

--- src/components/info-section.test.js
+++ src/components/info-section.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { InfoSection } from './info-section';
+import InfoSection from './info-section';
 
 describe('<InfoSection />', () => {
   it('Renders without crashing', () => {

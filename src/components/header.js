25ca528178496a0531bb771d5052861cdbfd44e5
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-06-03 23:39:17 -0500

    refactored header

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,7 @@ import './header.css';
 export default function Header(props) {
   return (
     <header>
-      <TopNav
-        onGenerateAuralUpdate={() => props.onGenerateAuralUpdate()}
-        onRestartGame={() => props.onRestartGame()}
-      />
+      <TopNav />
       <h1>HOT or COLD</h1>
     </header>
   );
